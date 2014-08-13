168c387b191e7331dae38cdb983943e0abaa0b38
Nikolay Kostyurin <jilizart@gmail.com>
2014-08-14 04:59:31 +0700

    png to base64, refactoring, only numbers filter

--- jquery.inputNumber.js
+++ jquery.inputNumber.js
@@ -2,29 +2,31 @@
  * @author Nikolay Kostyurin <jilizart@gmail.com>
  *     HTML5 Like enumerable input
  */
-(function($){
-    "use strict";
+(function($) {
+    'use strict';
 
-    var InputNumber = function(options, $el) {
-        this.$el = $el;
-        this.options = $.extend( {}, this.defaults, options );
+    var InputNumber = function(el, options) {
+        this.el = el;
+        this.$el = $(el);
+        this.options = $.extend({}, this.defaults, options);
         this.init();
     };
 
     InputNumber.prototype = {
 
+        el: null, // input element
         $el: null, // input element
         $wrap: null, //div wrapper element
 
         options: null,
         defaults: {
-            negative: true,
-            positive: true,
-            wrapClass: 'ranged-input',
-            upClass: 'up',
-            upTitle: 'Incrase',
-            downClass: 'down',
-            downTitle: 'Decrace'
+            'negative': true,
+            'positive': true,
+            'wrapClass': 'ranged-input',
+            'upClass': 'up',
+            'upTitle': 'incrase',
+            'downClass': 'down',
+            'downTitle': 'decrace'
         },
 
         init: function() {
@@ -42,69 +44,98 @@
 
         bindEvents:function() {
             var opts = this.options,
+                $el = this.$el,
                 self = this;
 
             this.$wrap
-            .delegate('a.'+opts.downClass,'click',function(e){
-                var defVal = parseInt(self.$el.val()) || 0,
-                    curVal = --defVal;
+                .delegate('a.' + opts.downClass, 'click', function(e) {
+                    self.down();
+                    e.preventDefault();
+                })
+                .delegate('a.' + opts.upClass, 'click', function(e) {
+                    self.up();
+                    e.preventDefault();
+                });
 
-                    console.log(curVal);
+            $el
+                .on('change', function(e) {
+                    if (e.currentTarget.value === '') e.currentTarget.value = 0;
+                })
+                .on('keypress', function(e) {
+                    var keyCode = window.event ? e.keyCode : e.which;
 
-                if(!opts.negative) {
-                    if(curVal >= 0) self.$el.val(curVal);
-                } else {
-                    self.$el.val(curVal);
+                    //codes for 0-9
+                    if (keyCode < 48 || keyCode > 57) {
+                        //codes for backspace, delete, enter
+                        if (keyCode !== 0 && keyCode !== 8 && keyCode !== 13 && keyCode !== 45 && !e.ctrlKey) {
+                            e.preventDefault();
+                        }
+                    }
+                })
+                .bind('mousewheel', function(e, delta) {
+                    var defVal = self.defaultElValue(),
+                        value = defVal + delta;
+
+                    if (! opts.negative) {
+                        if (value >= 0) {
+                            e.currentTarget.value = value;
+                        }
+                    } else if (! opts.positive) {
+                        if (value <= 0) {
+                            e.currentTarget.value = value;
+                        }
+                    } else {
+                        e.currentTarget.value = value;
+                    }
+                });
+        },
+
+        defaultElValue: function() {
+            return parseInt(this.el.value) || 0;
+        },
+
+        incrementElValue: function() {
+            var val = this.defaultElValue();
+            return ++ val;
+        },
+
+        decrementElValue: function() {
+            var val = this.defaultElValue();
+            return -- val;
+        },
+
+        up: function() {
+            var value = this.incrementElValue();
+
+            if (! this.options.positive) {
+                if (value <= 0) {
+                    this.el.value = value;
                 }
-                    
-                e.preventDefault();
-            })
-            .delegate('a.'+opts.upClass,'click',function(e){
-                var defVal = parseInt(self.$el.val()) || 0,
-                    curVal = ++defVal;
+            } else {
+                this.el.value = value;
+            }
+        },
 
-                    console.log(curVal);
+        down: function() {
+            var value = this.decrementElValue();
 
-                if(!opts.positive) {
-                    if(curVal <= 0) self.$el.val(curVal);
-                } else {
-                    self.$el.val(curVal);
+            if (! this.options.negative) {
+                if (value >= 0) {
+                    this.el.value = value;
                 }
-                    
-                e.preventDefault();
-            });
-
-            this.$el
-            .on('change',function(e){
-                var $this = $(this),
-                    val = $this.val();
-
-               if(val == '') $this.val(0);
-            })
-            .bind('mousewheel', function(e, delta) {
-                var defVal = parseInt(self.$el.val()) || 0,
-                    curVal = defVal+delta;
-
-                if(!opts.negative) {
-                    if(curVal >= 0) self.$el.val(curVal);
-                } else if(!opts.positive) {
-                    if(curVal <= 0) self.$el.val(curVal);
-                } else {
-                    self.$el.val(curVal);
-                }
-                    
-            });
-
+            } else {
+                this.el.value = value;
+            }
         }
 
     };
 
     $.fn.inputNumber = function(options) {
-
-        $(this).each(function(){
-            var number = new InputNumber(options, $(this));
+        return this.each(function() {
+            if (! $.data(this, 'inputNumber')) {
+                $.data(this, 'inputNumber', new InputNumber(this, options));
+            }
         });
-
     };
 
 })(jQuery);
